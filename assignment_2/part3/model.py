e68c555e2127063d01ff6cb2f9db7e877eb1ea87
Michelle Appel <appel.michelle@gmail.com>
2018-09-26 15:50:13 +0200

    lstm multiple layers

--- assignment_2/part3/model.py
+++ assignment_2/part3/model.py
@@ -31,58 +31,86 @@ class TextGenerationModel(nn.Module):
 
         self.num_hidden = lstm_num_hidden
         self.batch_size = batch_size
+        self.seq_length = seq_length
         self.vocabulary_size = vocabulary_size
+        self.lstm_num_layers = lstm_num_layers
 
-        self.W_gx = nn.Parameter(torch.randn(vocabulary_size, lstm_num_hidden)) # modulation gate input weight matrix
-        self.W_gh = nn.Parameter(torch.randn(lstm_num_hidden, lstm_num_hidden)) # modulation gate hidden weight matrix
+        self.W_gx = nn.ParameterList()
+        self.W_gh = nn.ParameterList()
 
-        self.W_ix = nn.Parameter(torch.randn(vocabulary_size, lstm_num_hidden)) # input gate input weight matrix
-        self.W_ih = nn.Parameter(torch.randn(lstm_num_hidden, lstm_num_hidden)) # input gate hidden weight matrix
+        self.W_ix = nn.ParameterList()
+        self.W_ih = nn.ParameterList()
 
-        self.W_fx = nn.Parameter(torch.randn(vocabulary_size, lstm_num_hidden)) # forget gate input weight matrix
-        self.W_fh = nn.Parameter(torch.randn(lstm_num_hidden, lstm_num_hidden)) # forget gate hidden weight matrix
-                     
-        self.W_ox = nn.Parameter(torch.randn(vocabulary_size, lstm_num_hidden )) # output gate input weight matrix
-        self.W_oh = nn.Parameter(torch.randn(lstm_num_hidden, lstm_num_hidden )) # output gate hidden weight matrix
+        self.W_fx = nn.ParameterList()
+        self.W_fh = nn.ParameterList()
 
-        self.W_px = nn.Parameter(torch.randn(vocabulary_size, vocabulary_size)) # prediction gate input weight matrix
-        self.W_ph = nn.Parameter(torch.randn(lstm_num_hidden, vocabulary_size)) # prediction gate hidden weight matrix
+        self.W_ox = nn.ParameterList()
+        self.W_oh = nn.ParameterList()
 
-        self.b_g = nn.Parameter(torch.zeros(lstm_num_hidden)) # modulation bias
-        self.b_i = nn.Parameter(torch.zeros(lstm_num_hidden)) # input bias
-        self.b_f = nn.Parameter(torch.zeros(lstm_num_hidden)) # forget bias
-        self.b_o = nn.Parameter(torch.zeros(lstm_num_hidden)) # output bias
-        self.b_p = nn.Parameter(torch.zeros(vocabulary_size)) # prediction bias
+        self.W_px = nn.ParameterList()
+        self.W_ph = nn.ParameterList()
+
+        self.b_g = nn.ParameterList()
+        self.b_i = nn.ParameterList()
+        self.b_f = nn.ParameterList()
+        self.b_o = nn.ParameterList()
+        self.b_p = nn.ParameterList()
+
+        for layer in range(lstm_num_layers):
+            self.W_gx.append(nn.Parameter(torch.randn((vocabulary_size, lstm_num_hidden)))) # modulation gate input weight matrix
+            self.W_gh.append(nn.Parameter(torch.randn((lstm_num_hidden, lstm_num_hidden)))) # modulation gate hidden weight matrix
+
+            self.W_ix.append(nn.Parameter(torch.randn((vocabulary_size, lstm_num_hidden)))) # input gate input weight matrix
+            self.W_ih.append(nn.Parameter(torch.randn((lstm_num_hidden, lstm_num_hidden)))) # input gate hidden weight matrix
+
+            self.W_fx.append(nn.Parameter(torch.randn((vocabulary_size, lstm_num_hidden)))) # forget gate input weight matrix
+            self.W_fh.append(nn.Parameter(torch.randn((lstm_num_hidden, lstm_num_hidden)))) # forget gate hidden weight matrix
+                        
+            self.W_ox.append(nn.Parameter(torch.randn((vocabulary_size, lstm_num_hidden)))) # output gate input weight matrix
+            self.W_oh.append(nn.Parameter(torch.randn((lstm_num_hidden, lstm_num_hidden)))) # output gate hidden weight matrix
+
+            self.W_px.append(nn.Parameter(torch.randn((vocabulary_size, vocabulary_size)))) # prediction gate input weight matrix
+            self.W_ph.append(nn.Parameter(torch.randn((lstm_num_hidden, vocabulary_size)))) # prediction gate hidden weight matrix
+
+            self.b_g.append(nn.Parameter(torch.zeros(lstm_num_hidden))) # modulation bias
+            self.b_i.append(nn.Parameter(torch.zeros(lstm_num_hidden))) # input bias
+            self.b_f.append(nn.Parameter(torch.zeros(lstm_num_hidden))) # forget bias
+            self.b_o.append(nn.Parameter(torch.zeros(lstm_num_hidden))) # output bias
+            self.b_p.append(nn.Parameter(torch.zeros(vocabulary_size))) # prediction bias
 
         self.tanh = nn.Tanh() # tanh module
         self.sigmoid = nn.Sigmoid() # softmax module
         self.softmax = nn.Softmax(dim=1) # softmax module
 
     def forward(self, x):
-        h = torch.zeros(self.batch_size, self.num_hidden) # last hidden state
-        c = torch.zeros(self.batch_size, self.num_hidden)
-        prediction = []
+        x_onehot = torch.FloatTensor(self.seq_length, self.batch_size, self.vocabulary_size)
+        x_onehot.zero_()
         
-        for x_t in torch.t(x): # loop through sequence
-            x_tonehot = torch.FloatTensor(self.batch_size, self.vocabulary_size)
-            x_tonehot.zero_()
+        for t, x_t in enumerate(torch.t(x)):
             ones = torch.ones(x_t.shape)
-            x_tonehot.scatter_(1,x_t.reshape(-1,1), ones.reshape(-1,1))
+            x_onehot[t].scatter_(1, x_t.reshape(-1,1), ones.reshape(-1,1))
 
-            x_t = x_tonehot
+        x = x_onehot
 
-            g_t = self.tanh(x_t @ self.W_gx + h @ self.W_gh + self.b_g) # modulation (equation (4))
-            i_t = self.sigmoid(x_t @ self.W_ix + h @ self.W_ih + self.b_i) # input (equation (5))
-            f_t = self.sigmoid(x_t @ self.W_fx + h @ self.W_fh + self.b_f) # forget (equation (6))
-            o_t = self.sigmoid(x_t @ self.W_ox + h @ self.W_oh + self.b_o) # output (equation (7))
+        for layer in range(self.lstm_num_layers):
+            h = torch.zeros(self.batch_size, self.num_hidden) # last hidden state
+            c = torch.zeros(self.batch_size, self.num_hidden) # last hidden cell state
+            prediction = []
 
-            c_t = g_t * i_t + c * f_t # cell (equation (8))
-            h_t = self.tanh(c_t) * o_t # hidden (equation (9))
+            for x_t in x: # loop through sequence
+                g_t = self.tanh(x_t @ self.W_gx[layer] + h @ self.W_gh[layer] + self.b_g[layer]) # modulation (equation (4))
+                i_t = self.sigmoid(x_t @ self.W_ix[layer] + h @ self.W_ih[layer] + self.b_i[layer]) # input (equation (5))
+                f_t = self.sigmoid(x_t @ self.W_fx[layer] + h @ self.W_fh[layer] + self.b_f[layer]) # forget (equation (6))
+                o_t = self.sigmoid(x_t @ self.W_ox[layer] + h @ self.W_oh[layer] + self.b_o[layer]) # output (equation (7))
 
-            c = c_t # store last cell
-            h = h_t # store last hidden state
+                c_t = g_t * i_t + c * f_t # cell (equation (8))
+                h_t = self.tanh(c_t) * o_t # hidden (equation (9))
 
-            p_t = h_t @ self.W_ph + self.b_p # prediction (equation (10))
-            y_t = self.softmax(p_t) # softmax (equation (11))
-            prediction.append(y_t)
+                c = c_t # store last cell
+                h = h_t # store last hidden state
+
+                p_t = h_t @ self.W_ph[layer] + self.b_p[layer] # prediction (equation (10))
+                y_t = self.softmax(p_t) # softmax (equation (11))
+                prediction.append(y_t)
+            x = prediction #torch.t(torch.stack(prediction))
         return prediction
